9f5e411c5ff2297ed92d8a39f7dcbec7ac69fcff
gc <chavezagreyson@gmail.com>
2026-03-24 10:58:10 -0700

    added crisis service interface and annotated crisis service implementation with service annotation

--- src/main/java/com/digitaltherapyassistant/service/CrisisService.java
+++ src/main/java/com/digitaltherapyassistant/service/CrisisService.java
@@ -2,18 +2,10 @@ package com.digitaltherapyassistant.service;
 
 import org.springframework.stereotype.Service;
 
+
+
+
 @Service
 public class CrisisService {
-
-    // get coping strategies
-
-    // get emergency/trusted contacts
-
-    // get safety plan
-
-    // update safety plan
-
-    // detect crisis
-
 
 }
---
+++ src/main/java/com/digitaltherapyassistant/service/interfaces/CrisisServiceInterface.java
@@ -0,0 +1,17 @@
+package com.digitaltherapyassistant.service.interfaces;
+
+
+public interface CrisisServiceInterface {
+
+    // get coping strategies
+
+    // get emergency/trusted contacts
+
+    // get safety plan
+
+    // update safety plan
+
+    // detect crisis
+
+
+}
