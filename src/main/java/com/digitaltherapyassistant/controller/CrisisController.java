5a0cef4e615dc1b7ba4ec67f8597006ec07dfd26
gc <chavezagreyson@gmail.com>
2026-03-23 17:53:07 -0700

    added spring open api dependency

--- src/main/java/com/digitaltherapyassistant/controller/CrisisController.java
+++ src/main/java/com/digitaltherapyassistant/controller/CrisisController.java
@@ -4,6 +4,8 @@ import com.digitaltherapyassistant.dto.response.ApiResponse;
 import com.digitaltherapyassistant.dto.response.CrisisResponse;
 import com.digitaltherapyassistant.mapper.DtoMapper;
 import com.digitaltherapyassistant.service.CrisisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
